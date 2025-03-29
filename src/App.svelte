d3bd0b544cae295b3a31663779423a9ce80fc29d
Nils Leijdekker <nilsleijdekker@gmail.com>
2025-03-29 09:28:39 +0100

    hi

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  let dirs: string[] = [] 
+  onMount(async () => { 
+    const res = await fetch('../lib/img');
+    dirs = await res.json()
+    console.log(await res.json());
+  })
+  function getImagePath() {
+    const index = Math.floor(Math.random() * dirs.length);
+    const img = dirs[index]; console.log(img);
+    return `../lib/img/${img}`;
+  }
+</script>
+
+<!-- svelte-ignore a11y_img_redundant_alt -->
+<img src={getImagePath()} alt="Random Image" />
+
+<style>
+</style>
